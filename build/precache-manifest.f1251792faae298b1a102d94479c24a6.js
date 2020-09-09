60d9e74c931f864b2f7805fd0b463252784366b0
nick <nickforbizz@gmail.com>
2020-09-09 10:42:08 +0300

    new changes build

---
+++ build/precache-manifest.f1251792faae298b1a102d94479c24a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a50006906b1a44d83bb1fc132c99b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91b6199436c656e28276",
+    "url": "/static/css/2.4dd4bf0b.chunk.css"
+  },
+  {
+    "revision": "280797cd0a3e817d1970",
+    "url": "/static/css/main.03488781.chunk.css"
+  },
+  {
+    "revision": "91b6199436c656e28276",
+    "url": "/static/js/2.f15e761a.chunk.js"
+  },
+  {
+    "revision": "3e69507558fa9ce07a3fd0cbca127a18",
+    "url": "/static/js/2.f15e761a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280797cd0a3e817d1970",
+    "url": "/static/js/main.c58010a5.chunk.js"
+  },
+  {
+    "revision": "7cec3c76c9d7b28dd876",
+    "url": "/static/js/runtime-main.cba2cd7a.js"
+  }
+]);
